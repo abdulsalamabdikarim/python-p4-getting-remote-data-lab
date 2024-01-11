5d2e37ca2f04b38826f1fe7d0297423b32348d1c
Abdulsalam Abdikarim <achalam00@gmail.com>
2024-01-11 18:40:21 +0300

    Completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,10 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data = json.loads(self.get_response_body())
+        # print(data)
+        return data
